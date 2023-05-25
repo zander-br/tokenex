198ab9448a58522aa35766dab5cb81bab14623bb
Anderson Santos <anderson.feitoza.santos@gmail.com>
2023-05-25 19:20:42 -0300

    chore: initial commit

---
+++ lib/tokenex.ex
@@ -0,0 +1,18 @@
+defmodule Tokenex do
+  @moduledoc """
+  Documentation for `Tokenex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tokenex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tokenex_test.exs
@@ -0,0 +1,8 @@
+defmodule TokenexTest do
+  use ExUnit.Case
+  doctest Tokenex
+
+  test "greets the world" do
+    assert Tokenex.hello() == :world
+  end
+end

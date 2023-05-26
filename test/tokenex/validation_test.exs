3f6575c5e8896307e9a6d4b77a95dc48528f00fe
Anderson Feitoza <anderson.feitoza.santos@gmail.com>
2023-05-26 17:22:15 -0300

    test: add utility to hlep validate options

---
+++ test/tokenex/validation_test.exs
@@ -0,0 +1,34 @@
+defmodule Tokenex.ValidationTest do
+  use ExUnit.Case, async: true
+  doctest Tokenex.Validation
+
+  alias Tokenex.Validation
+
+  describe "validate/2" do
+    test "should return :ok when the validation is successful" do
+      opts = [{:name, "tokenex"}]
+      validator = fn {:name, name} when is_binary(name) -> :ok end
+      assert :ok == Validation.validate(opts, validator)
+    end
+
+    test "should return {:error, reason} when validation fails" do
+      opts = [{:name, "tokenex"}]
+      validator = fn _opt -> {:error, "any_reason"} end
+      assert {:error, "any_reason"} == Validation.validate(opts, validator)
+    end
+  end
+
+  describe "validate!/2" do
+    test "should return :ok when the validation is successful" do
+      opts = [{:name, "tokenex"}]
+      validator = fn {:name, name} when is_binary(name) -> :ok end
+      assert :ok == Validation.validate!(opts, validator)
+    end
+
+    test "should return a raise when validation fails" do
+      opts = [{:name, "tokenex"}]
+      validator = fn _opt -> {:error, "any_reason"} end
+      assert_raise ArgumentError, "any_reason", fn -> Validation.validate!(opts, validator) end
+    end
+  end
+end

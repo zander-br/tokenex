8864d8b1c3c5ffcbaea66933d8f6d0e2357dbe74
Anderson Santos <anderson.feitoza.santos@gmail.com>
2023-10-02 20:06:24 -0300

    feat: use tokenex cache

--- lib/tokenex.ex
+++ lib/tokenex.ex
@@ -1,18 +1,32 @@
 defmodule Tokenex do
-  @moduledoc """
-  Documentation for `Tokenex`.
-  """
+  use Supervisor
 
-  @doc """
-  Hello world.
+  alias Tokenex.{Cache, Config, Registry}
+  alias __MODULE__
 
-  ## Examples
+  @type option :: {:adapters, [module() | {module() | Keyword.t()}]}
 
-      iex> Tokenex.hello()
-      :world
+  def start_link(_opts) do
+    opts = Application.get_all_env(:tokenex)
+    config = Config.new(opts)
+    Supervisor.start_link(Tokenex, config, name: Tokenex)
+  end
 
-  """
-  def hello do
-    :world
+  @impl Supervisor
+  def init(%Config{adapters: adapters}) do
+    children = Enum.map(adapters, &adapter_child_spec/1)
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+
+  defp adapter_child_spec({adapter, opts}) do
+    name = Registry.via(adapter)
+    opts = Keyword.merge(opts, adapter: adapter, name: name)
+    Supervisor.child_spec({Cache, opts}, id: adapter)
+  end
+
+  defp adapter_child_spec(adapter) do
+    name = Registry.via(adapter)
+    opts = [{:adapter, adapter}, {:name, name}]
+    Supervisor.child_spec({Cache, opts}, id: adapter)
   end
 end
